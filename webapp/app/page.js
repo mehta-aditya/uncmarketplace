f5a07ff1e301a94bbb2e3b9a4f812c7e98cd2105
ryogarajan5594 <77201941+ryogarajan5594@users.noreply.github.com>
2024-11-02 20:21:19 -0400

    Updated by adding signUp function

--- webapp/app/page.js
+++ webapp/app/page.js
@@ -3,11 +3,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <h1>HERO HOMEPAGE</h1>
-      <a href="/signin">Sign In</a>
-      <br />
-      <a href="/signup">Sign Up</a>
-    </>
+    
   );
 }
+
+
+//<>
+  //    <h1>HERO HOMEPAGE</h1>
+   //   <a href="/signin">Sign In</a>
+    //  <br />
+    //  <a href="/signup">Sign Up</a>
+      
+   // </>

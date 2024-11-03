37eb4bed77d4323e9d8eabe80129cc7ea523921b
lukenovak2004 <49239616+lukenovak2004@users.noreply.github.com>
2024-11-03 01:23:59 -0500

    adding more pages

--- webapp/app/homepage/page.js
+++ webapp/app/homepage/page.js
@@ -1,5 +1,5 @@
 <body>
      <h1>Welcome to Tar Heel Trade!</h1>
      <h2>Click <a href="/signin">here</a> to sign in!</h2>
-     <div class="picbg"><img src="rammybg.png" alt="rammy" ></div>
+     <div class="picbg"><img src="rammybg.png" alt="rammy"/></div>
 </body>

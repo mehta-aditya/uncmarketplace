8cb0b66a87f64e7525b2f2e67ae6f0604be813a5
Aditya Mehta <59856464+mehta-aditya@users.noreply.github.com>
2024-11-03 01:47:40 -0500

    clothing commit

--- webapp/app/listing/page.js
+++ webapp/app/listing/page.js
@@ -8,7 +8,7 @@ function Page() {
   const [image, setImage] = useState();
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("Clothing");
   const { user } = useAuthContext();
 
   const handleForm = async (event) => {
@@ -24,90 +24,93 @@ function Page() {
   };
 
   return (
-<main>
-  <h1>Tar Heel Trade</h1>
-          <hr></hr>
-  <header>   
-    <nav>
-        <div id="sidenav">
+    <main>
+      <h1>Tar Heel Trade</h1>
+      <hr></hr>
+      <header>
+        <nav>
+          <div id="sidenav">
             <ul>
-                <li><a href="/marketplace">Home</a></li>
-                <li><a href="#">Sign Out</a></li>
-
+              <li>
+                <a href="/marketplace">Home</a>
+              </li>
+              <li>
+                <a href="#">Sign Out</a>
+              </li>
             </ul>
+          </div>
+        </nav>
+      </header>
+      <div>
+        <div className="container">
+          <form onSubmit={handleForm}>
+            <div className="">
+              <h1 id="title" className="text-center font-extrabold text-2xl">
+                Design Your Post{" "}
+              </h1>
+              <p id="description" className="description text-center"></p>
+              <input
+                type="file"
+                multiple={true}
+                onChange={(e) => setImage(e.target.files[0])}
+                required
+              />
+            </div>
+
+            <div className="userprice">
+              <label id="number-label" required>
+                Price
+              </label>
+              <input
+                type="number"
+                onChange={(e) => setPrice(e.target.value)}
+                name="price"
+                id="number"
+                min="0"
+                max="99999"
+                className="form-control"
+                placeholder="30"
+                required
+              />
+            </div>
+
+            <div className="userinput">
+              <p>Item Category?</p>
+              <select
+                id="dropdown"
+                name="role"
+                className="form-control"
+                onChange={(e) => setCategory(e.target.value)}
+                required
+              >
+                <option disabled value>
+                  Select an option
+                </option>
+                <option>Clothing</option>
+                <option>Technology</option>
+                <option>School Supplies</option>
+                <option>Miscellaneous</option>
+              </select>
+            </div>
+
+            <div className="userinput">
+              <p>Description?</p>
+              <textarea
+                className="input-textarea"
+                name="comment"
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="2020 Apple Macbook Air; Gently used; 16gb 256 ssd. Contact #: 704-xxx-xxx"
+                required
+              ></textarea>
+            </div>
+            <div className="userinput">
+              <button type="submit" id="submit" className="submit-button">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    </nav>
-    </header>
-    <div>
-      <div className="container">
-        <form onSubmit={handleForm}>
-          <div className="">
-            <h1 id="title" className="text-center font-extrabold text-2xl">
-              Design Your Post{" "}
-            </h1>
-            <p id="description" className="description text-center"></p>
-            <input
-              type="file"
-              multiple={true}
-              onChange={(e) => setImage(e.target.files[0])}
-              required
-            />
-          </div>
-
-          <div className="userprice">
-            <label id="number-label" required>
-              Price
-            </label>
-            <input
-              type="number"
-              onChange={(e) => setPrice(e.target.value)}
-              name="price"
-              id="number"
-              min="0"
-              max="99999"
-              className="form-control"
-              placeholder="30"
-              required
-            />
-          </div>
-
-          <div className="userinput">
-            <p>Item Category?</p>
-            <select
-              id="dropdown"
-              name="role"
-              className="form-control"
-              onChange={(e) => setCategory(e.target.value)}
-              required
-            >
-              <option disabled value>
-                Select an option
-              </option>
-              <option>Clothing</option>
-              <option>Technology</option>
-              <option>School Supplies</option>
-              <option>Miscellaneous</option>
-            </select>
-          </div>
-
-          <div className="userinput">
-            <p>Description?</p>
-            <textarea
-              className="input-textarea"
-              name="comment"
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="2020 Apple Macbook Air; Gently used; 16gb 256 ssd. Contact #: 704-xxx-xxx"
-              required
-            ></textarea>
-          </div>
-          <div className="userinput">
-            <button type="submit" id="submit" className="submit-button">
-              Submit
-            </button>
-          </div>
-        </form>
       </div>
-    </div>
     </main>
   );
 }
